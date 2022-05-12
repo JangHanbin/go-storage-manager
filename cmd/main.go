c8c51a60a517fff58d947ba677c7708e1fd5fa00
Mac <mac@Macui-MacBookPro-5.local>
2022-05-12 17:57:16 +0900

    FEAT: Add urfave cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-	"io/ioutil"
+	"github.com/janghanbin/go-storage-manager/configs"
+	"github.com/urfave/cli/v2"
 	"log"
-	"net/http"
+	"os"
 )
 
 func main() {
-	resp, err := http.Get("http://test.gilgil.net")
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "config",
+				Aliases:  []string{"c"},
+				Usage:    "Load configuration from `FILE`",
+				Required: true,
+			},
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		configs.Cfg = configs.ReadConfiguration("configs/config.json")
+		return nil
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	println(string(body))
 }
